ae100945fbafccb47cc9452ccc7f725e4229f454
d-harel <adopt_batata@yahoo.com>
2020-07-16 15:23:24 +0300

    Added function to disable admin bar for specific users

--- twentytwentychild/README.md
+++
@@ -1,3 +0,0 @@
-# twentytwentychild
-
-WordPress Development Test
--- twentytwentychild/functions.php
+++ twentytwentychild/functions.php
@@ -1,7 +1,22 @@
 <?php
 
-function twentytwentychild_register_styles()
+// Enqueue Scripts
+function ttc_register_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
-add_action('wp_enqueue_scripts', 'twentytwentychild_register_styles');
+add_action('wp_enqueue_scripts', 'ttc_register_styles');
+
+// Remove Admin Bar for specific users
+function ttc_show_admin_bar($show)
+{
+    // For future use - add user names here
+    $remove_for_users = ['wp-test'];
+    $user = wp_get_current_user();
+    if (in_array($user->user_login, $remove_for_users)) {
+        $show = false;
+    }
+    return $show;
+}
+
+add_filter('show_admin_bar', 'ttc_show_admin_bar');
